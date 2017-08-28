2fbc2cfd615edad1441cf147b5fc7df3201c016b
danny perez <perez6736@gmail.com>
2017-08-27 20:51:30 -0400

    check to see if guess is correct or not

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,14 +9,34 @@ var words =['Cardinals','Falcons','Ravens','Bills','Panthers','Bears','Bengals',
 
 //the game word. Takes a random number between 0 and length of array and uses that number to select what index of the array to pick. 
 var gameWord = words[Math.floor(Math.random()*words.length)];
+//make the game word lowercase
+gameWord = gameWord.toLowerCase();
 
 // keeps tracks of letters already guessed. 
 var guessedLetters = [];
 
-
+//split the gameWord to an array
+var gameWordArr = gameWord.split("");
 
 // this is ran whenever a key is pressed - event.key will be the key pressed. 
 document.onkeyup = function(event){
-	console.log("Game as started.");
+	// store the letter the user guessed. 
+	var letterGuessed = event.key;
+	console.log(letterGuessed);
+
+	// loop through the word to check if the key pressed matches a letter in the word.
+	for(i=0;i<gameWordArr.length;i++){
+		if(event.key == gameWordArr[i]){
+			console.log("correct guess.");
+			return;
+		}
+	}
+	console.log("wrong guess.");
+
+
+
+
+
 }
+
 
